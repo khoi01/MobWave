1ef80f8eae50c427f9c1e0e0400e44fce6161618
BuildTools <unconfigured@null.spigotmc.org>
2019-07-09 19:36:07 +0800

    add file

--- src/khoi/mobwave/controller/CommandWaveEvent.java
+++ src/khoi/mobwave/controller/CommandWaveEvent.java
@@ -3,13 +3,42 @@ package khoi.mobwave.controller;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandWaveEvent implements CommandExecutor {
 
+	//https://www.youtube.com/watch?v=jytF-6WD2Eo  (setup permissions)
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		// TODO Auto-generated method stub
-		return false;
+
+
+		if(!(sender instanceof Player)) {
+			return true;
+		}
+		
+		Player player = (Player) sender;
+		
+		if(args == null) {
+			return true;
+		}
+		
+		if(args.length <= 0) {
+			return true;
+		}
+		
+		switch(args[0]) {
+		case "start":
+			player.sendMessage("you used (mobwave start)");
+			
+			if(args[1] == "war") {
+				player.sendMessage("you used (mobwave start war)");
+			}
+			break;
+		default:
+			player.sendMessage("none used any command");
+		}
+		
+		return true;
 	}
 
 }
--- src/khoi/mobwave/controller/Main.java
+++ src/khoi/mobwave/controller/Main.java
@@ -1,31 +1,18 @@
 package khoi.mobwave.controller;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import khoi.customcommand.CommandTps;
-import khoi.mobwave.model.ListWave;
-import khoi.mobwave.model.Mob;
-import khoi.mobwave.model.Section;
-import khoi.mobwave.model.Wave;
+
 
 
 public class Main extends JavaPlugin{
-	//https://bukkit.gamepedia.com/Configuration_API_Reference
-//https://bukkit.org/threads/tut-bukkits-new-fileconfiguration-api-create-a-yaml-configuration.42775/
-  //https://www.spigotmc.org/threads/creating-a-yml-config-if-it-does-not-exist.126091/
-	public static File mainconfig;
-
     
     @Override
 	public void onEnable() {
-			
-
-		InitConfig();
-		getCommand("startwave").setExecutor(new CommandWaveEvent());
+		
+    	Globals.InitConfig(this);
+    	this.getCommand("mobwave").setExecutor(new CommandWaveEvent());
 
     }
     
@@ -33,79 +20,6 @@ public class Main extends JavaPlugin{
 	public void onDisable() {
 		saveConfig();
 	}
-	
-	
-	public void InitConfig() {
-        mainconfig = new File(getDataFolder(), "config.yml");
 
-        if(!mainconfig.exists()) {
-            getConfig().options().copyDefaults(true);
-			getConfig().set("status",true);
-			
-			   /* Section 1*/
-
-			Mob Mob1 = new Mob("Zombie",2);
-			Mob Mob2 = new Mob("Spider",2);
-
-			Mob Mob3 = new Mob("Orc",2);
-			Mob Mob4 = new Mob("Zombie",2);
-			List<Mob> mobs1 = new ArrayList<Mob>();
-			mobs1.add(Mob1);
-			mobs1.add(Mob2);
-			
-			List<Mob> mobs2 = new ArrayList<Mob>();
-			mobs2.add(Mob3);
-			mobs2.add(Mob4);	
-			
-			Wave wave1 = new Wave(1,false,false,"random",mobs1);
-			Wave wave2 = new Wave(2,false,false,"random",mobs2);
-
-		    List<Wave> waves1 = new ArrayList<Wave>();
-		    waves1.add(wave1);
-		    waves1.add(wave2);
-		    
-			   /* Section 2*/
-
-			Mob Mob5 = new Mob("Creeper",3);
-			Mob Mob6 = new Mob("Creeper",2);
-
-			Mob Mob7 = new Mob("Atcher",2);
-			Mob Mob8 = new Mob("Boomer",2);
-			
-			
-			List<Mob> mobs3 = new ArrayList<Mob>();
-			mobs3.add(Mob5);
-			mobs3.add(Mob6);
-			
-			List<Mob> mobs4 = new ArrayList<Mob>();
-			mobs4.add(Mob7);
-			mobs4.add(Mob8);
-			
-			Wave wave3 = new Wave(1,false,false,"random",mobs3);
-			Wave wave4 = new Wave(2,false,false,"random",mobs4);
-
-		    List<Wave> waves2 = new ArrayList<Wave>();
-		    waves2.add(wave3);
-		    waves2.add(wave4);
-		    
-		    Section section1 = new Section("War 1",waves1);
-		    Section section2 = new Section("War 2",waves2);
-
-		    List<Section> sections = new ArrayList<Section>();
-		    sections.add(section1);
-		    sections.add(section2);
-
-		    ListWave list = new ListWave(sections);
-		    
-		    getConfig().set("Data", list);
-		    
-			saveConfig();
-        }		
-	}
-	
-    
-
-    
- 
 	
 }
